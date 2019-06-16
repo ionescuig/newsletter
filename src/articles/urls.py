a7a8816b08289bf1a4c22836b30f35df387d0914
Gabriel <ionescuig@yahoo.com>
2019-06-17 00:56:13 +0100

    Added publish article view; only the author can publish. Added comments and docstrings to views in articles app.

--- src/articles/urls.py
+++ src/articles/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ArticlesCreateView, ArticlesDetailView, ArticlesListView, ArticlesUpdateView
+from .views import ArticlesCreateView, ArticlesDetailView, ArticlesListView, ArticlesUpdateView, publish_article_view
 
 urlpatterns = [
     path('all', ArticlesListView.as_view(), name='list'),
     path('new', ArticlesCreateView.as_view(), name='create'),
-    path('update/<str:slug>', ArticlesUpdateView.as_view(), name='update'),
+    path('<str:slug>/publish', publish_article_view, name='publish'),
+    path('<str:slug>/update', ArticlesUpdateView.as_view(), name='update'),
     path('<str:slug>', ArticlesDetailView.as_view(), name='detail'),
 ]
--- src/articles/views.py
+++ src/articles/views.py
@@ -1,12 +1,40 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.utils import timezone
 
 from .forms import ArticleForm
 from .models import Article
 
 
+def publish_article_view(request, *args, **kwargs):
+    """
+    If the user is also the author, publish the article.
+    If there is no published_date, will set one using timezone.now().
+    Redirects to article detail page or 'Page Not Found'.
+    """
+    # retrieving the article to be published
+    articles = Article.objects.filter(slug__iexact=kwargs['slug'])
+    article = articles[0]
+
+    # check if the user is the author
+    if request.user == article.author:
+        article.published = True
+        # set a published_date if there is none
+        if not article.published_date:
+            article.published_date = timezone.now()
+        article.save()
+        return redirect(article.get_absolute_url())
+    # if the user is not the author raise Page Not Found error
+    else:
+        raise Http404()
+
+
 class HomePageView(ListView):
+    """
+    A list of published articles (from all users) ordered by date, from the newest one to the oldest.
+    """
     model = Article
     template_name = 'articles/list.html'
 
@@ -15,6 +43,9 @@ class HomePageView(ListView):
 
 
 class ArticlesCreateView(LoginRequiredMixin,CreateView):
+    """
+    Create a new article using the user as the author.
+    """
     form_class = ArticleForm
     template_name = 'articles/create.html'
 
@@ -25,19 +56,34 @@ class ArticlesCreateView(LoginRequiredMixin,CreateView):
 
 
 class ArticlesDetailView(DetailView):
+    """
+    Article page.
+    Uses the template to display different things to author and other users.
+    """
     model = Article
     template_name = 'articles/detail.html'
 
 
 class ArticlesListView(LoginRequiredMixin, ListView):
+    """
+    A list of user's articles,
+    drafts first, and then published,
+    ordered by published_date, and then by updated_date,
+    from the newest one to the oldest.
+    """
     model = Article
     template_name = 'articles/list.html'
 
     def get_queryset(self):
-        return Article.objects.filter(author=self.request.user).order_by('published', '-published_date')
+        return Article.objects.filter(author=self.request.user).order_by('published',
+                                                                         '-published_date',
+                                                                         '-updated_date')
 
 
 class ArticlesUpdateView(UserPassesTestMixin, UpdateView):
+    """
+    Article update view if the user is also the author.
+    """
     form_class = ArticleForm
     template_name = 'articles/update.html'
 
@@ -46,4 +92,3 @@ class ArticlesUpdateView(UserPassesTestMixin, UpdateView):
 
     def get_queryset(self):
         return Article.objects.all()
-
